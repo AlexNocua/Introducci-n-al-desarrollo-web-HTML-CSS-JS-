8b42fe1df23520e0340c9b8d25ccaffdd29f86b9
AlexNocua <nocua68@gmail.com~>
2024-05-13 23:38:31 -0500

    Actualizacion del material

---
+++ practica 1/script.js
@@ -0,0 +1,44 @@
+// const fondo = document.querySelector('.img')
+// const imgMia = document.querySelector('.imagenMia')
+
+
+// i = 0
+// fondo.onclick = function () {
+//     i += 1
+//     if (i % 2 == 0) {
+//         fondo.style.transition = 'all 0.2s'
+//         fondo.style.opacity = '0'
+//         imgMia.style.transform = 'scale(2)'
+
+//     }
+//     else {
+//         fondo.style.transition = 'all 0.2s'
+//         fondo.style.opacity = '1'
+
+//     }
+// }
+
+// function rotarImagen() {
+//     j += 1
+//     if (j % 2 == 0) {
+//         imgMia.style.transition = 'all 0.2s'
+//         imgMia.style.transform = 'rotate(360deg)'
+
+//     }
+//     else {
+//         imgMia.style.transition = 'all 0.2s'
+//         imgMia.style.transform = 'rotate(0deg)'
+
+//     }
+
+// }
+
+
+
+// j = 0
+// imgMia.addEventListener('click', rotarImagen)
+
+// // tengo un nuevo usuario y una nueva persona
+// // ana es administrativa
+// profetion = document.querySelector('.profecion')
+// profetion.innerText = 'Administrador'
---
+++ practica 2/script.js
@@ -0,0 +1,121 @@
+// let personas = {
+//     1: {
+//         nombre: 'Alex',
+//         especialidades: ['Diseño gráfico', 'Desarrollo web'],
+//         correo: 'alex@example.com'
+//     },
+//     2: {
+//         nombre: 'María',
+//         especialidades: ['Marketing digital', 'Redes sociales'],
+//         correo: 'maria@example.com'
+//     },
+//     3: {
+//         nombre: 'Juan',
+//         especialidades: ['Programación', 'Inteligencia Artificial'],
+//         correo: 'juan@example.com'
+//     },
+//     4: {
+//         nombre: 'Carlos',
+//         especialidades: ['Diseño de interfaces', 'Experiencia de usuario'],
+//         correo: 'carlos@example.com'
+//     },
+//     5: {
+//         nombre: 'Ana',
+//         especialidades: ['Desarrollo móvil', 'Diseño UX/UI'],
+//         correo: 'ana@example.com'
+//     },
+//     6: {
+//         nombre: 'Pedro',
+//         especialidades: ['SEO', 'SEM'],
+//         correo: 'pedro@example.com'
+//     },
+//     7: {
+//         nombre: 'Laura',
+//         especialidades: ['Copywriting', 'Edición de contenido'],
+//         correo: 'laura@example.com'
+//     },
+//     8: {
+//         nombre: 'Sofía',
+//         especialidades: ['Gestión de proyectos', 'Planificación estratégica'],
+//         correo: 'sofia@example.com'
+//     },
+//     9: {
+//         nombre: 'Diego',
+//         especialidades: ['Desarrollo frontend', 'Diseño de interfaces'],
+//         correo: 'diego@example.com'
+//     },
+//     10: {
+//         nombre: 'Elena',
+//         especialidades: ['Análisis de datos', 'Business Intelligence'],
+//         correo: 'elena@example.com'
+//     },
+//     11: {
+//         nombre: 'Luis',
+//         especialidades: ['Desarrollo backend', 'Base de datos'],
+//         correo: 'luis@example.com'
+//     },
+//     12: {
+//         nombre: 'Marta',
+//         especialidades: ['Diseño de marca', 'Identidad visual'],
+//         correo: 'marta@example.com'
+//     },
+//     13: {
+//         nombre: 'Jorge',
+//         especialidades: ['Marketing de contenidos', 'SEO'],
+//         correo: 'jorge@example.com'
+//     },
+//     14: {
+//         nombre: 'Lucía',
+//         especialidades: ['Diseño editorial', 'Maquetación'],
+//         correo: 'lucia@example.com'
+//     },
+//     15: {
+//         nombre: 'Pablo',
+//         especialidades: ['Growth Hacking', 'CRO'],
+//         correo: 'pablo@example.com'
+//     },
+//     16: {
+//         nombre: 'Carmen',
+//         especialidades: ['Redacción creativa', 'Storytelling'],
+//         correo: 'carmen@example.com'
+//     },
+//     17: {
+//         nombre: 'Mario',
+//         especialidades: ['Desarrollo móvil', 'Programación'],
+//         correo: 'mario@example.com'
+//     },
+//     18: {
+//         nombre: 'Natalia',
+//         especialidades: ['Diseño UX/UI', 'Prototipado'],
+//         correo: 'natalia@example.com'
+//     },
+//     19: {
+//         nombre: 'Roberto',
+//         especialidades: ['Diseño de producto', 'Innovación'],
+//         correo: 'roberto@example.com'
+//     },
+//     20: {
+//         nombre: 'Eva',
+//         especialidades: ['Marketing digital', 'SEM'],
+//         correo: 'eva@example.com'
+//     }
+// };
+
+
+
+// const container = document.querySelector('.container');
+
+
+// function crearElementos(persona) {
+//     const tarjeta = document.createElement('div')
+//     tarjeta.classList.add('tarjeta')
+//     const titulo = document.createElement('h1')
+//     titulo.innerHTML = `Hola mis niños, los saluda ${persona["nombre"]}`;
+//     tarjeta.append(titulo)
+//     document.body.appendChild(tarjeta)
+// }
+
+// for (let persona in personas) {
+//     console.log(personas[persona]);
+//     crearElementos(personas[persona])
+// }
